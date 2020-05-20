65a65592459b371c482153c9b1b278159117496e
Ulkuerdogdu <64045228+Ulkuerdogdu@users.noreply.github.com>
2020-05-20 17:00:47 -0400

    Yeni deneme

--- src/Tiral.java
+++ src/Tiral.java
@@ -1,5 +1,12 @@
+import java.sql.SQLOutput;
+
 public class Tiral {
     public static void main(String[] args) {
         System.out.println("Hello Atlanta");
+        int a= 9;
+        String str="Merhaba";
+        System.out.println(str + a);
+
+
     }
 }
