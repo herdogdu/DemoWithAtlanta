08109331aa5ae859960fc2046ebcd24fe7c684bc
tugba <ttorun16@gmail.com>
2020-05-20 19:04:59 -0400

    dddddmm

--- src/Tiral.java
+++ src/Tiral.java
@@ -1,5 +1,22 @@
+package src;
+
 public class Tiral {
+
     public static void main(String[] args) {
+
+
+        System.out.println("Hello Atlantabbbb");
+
+        System.out.println("merhababbb");
+        System.out.println(" ");
+
+        System.out.println("---------------------");
         System.out.println("Hello Atlanta");
+
+        System.out.println("Hello Everyone :))");
+
+        System.out.println("hi");
+
+
     }
 }
